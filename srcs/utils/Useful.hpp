2056af3d4822289606c830b8035c6268a78b51e6
wyoheiii <seikimatu4649@gmail.com>
2022-09-24 15:12:54 +0900

    difine hpp

---
+++ srcs/utils/Useful.hpp
@@ -0,0 +1,16 @@
+#ifndef Useful_HPP
+#define Useful_HPP
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <set>
+#define string std::string
+#define cout std::cout
+#define endl std::endl
+#define cerr std::cerr
+#define flush std::flush
+#define vector std::vector
+#define map std::map
+#define set std::set
+#endif // !Useful
