75fe3dd037079e3df5795be63849e32c27ebdb64
Akihide Ito <akihide.ito.dev@gmail.com>
2022-11-27 14:51:56 +0900

    HttpProcessorクラスのテストの修正

--- tests/unit_test/HttpProcessorTest.cc
+++ tests/unit_test/HttpProcessorTest.cc
@@ -3,7 +3,7 @@
 #include "HttpProcessor.hpp"
 #include "HttpResponse.hpp"
 
-TEST(HttpProcessor, test) {
+TEST(HttpProcessor, ProcessHttpRequest) {
   std::map<std::string, LocationContext> location_contexts;
 
   LocationContext location_context;
@@ -13,26 +13,33 @@ TEST(HttpProcessor, test) {
 
   std::string result;
 
-  ParsedRequest parsed_request;
+  {
+    ParsedRequest parsed_request;
 
-  parsed_request.m = kGet;
-  parsed_request.request_path = "/";
-  parsed_request.version = "HTTP/1.1";
+    parsed_request.m = kGet;
+    parsed_request.request_path = "/";
+    parsed_request.version = "HTTP/1.1";
 
-  HttpProcessor::ProcessHttpRequest(parsed_request, location_contexts, &result);
-
-  HttpResponse http_response;
-  http_response.SetStatusCode(200);
-  http_response.SetHeader("Content-Type", "text/html");
-  File file("./html/sample.html");
-  std::vector<std::string> file_contents = file.StoreFileLinesInVec();
-
-  std::string body;
-  for (size_t i = 0; i < file_contents.size(); ++i) {
-    body += file_contents[i];
+    HttpProcessor::ProcessHttpRequest(parsed_request, location_contexts,
+                                      &result);
   }
-  http_response.SetBody(body);
-  http_response.SetHeader("Content-Length", std::to_string(body.size()));
 
-  EXPECT_EQ(result, http_response.GetRawResponse());
+  std::string expected;
+  {
+    HttpResponse http_response;
+    http_response.SetStatusCode(200);
+    http_response.SetHeader("Content-Type", "text/html");
+    File file("./html/sample.html");
+    std::vector<std::string> file_contents = file.StoreFileLinesInVec();
+
+    std::string body;
+    for (size_t i = 0; i < file_contents.size(); ++i) {
+      body += file_contents[i];
+    }
+    http_response.SetBody(body);
+    http_response.SetHeader("Content-Length", std::to_string(body.size()));
+    expected = http_response.GetRawResponse();
+  }
+
+  EXPECT_EQ(result, expected);
 }
