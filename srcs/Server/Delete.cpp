4ce0264c3d28601c2388393d1e8282b91f1c98df
wyoheiii <seikimatu4649@gmail.com>
2022-12-24 16:22:02 +0900

    delete post 条件変更

--- srcs/Server/Delete.cpp
+++ srcs/Server/Delete.cpp
@@ -7,6 +7,10 @@ void Delete::Run(const std::string &path, Socket *sock) {
   std::string file_path = path;
   if (path[path.size() - 1] == '/') file_path + sock->location_context.index;
   File f(file_path);
+  if (!f.IsExist()) {
+    rescode_ = kKk404NotFound;
+    return;
+  }
   if (f.IsDir()) {
     rescode_ = kKk403Forbidden;
     return;
@@ -14,7 +18,7 @@ void Delete::Run(const std::string &path, Socket *sock) {
   if (f.DelFile() == -1) {
     rescode_ = kKk403Forbidden;
   } else {
-    rescode_ = kKk204NoContent;
+    rescode_ = kKk200Ok;
   }
 }
 void Delete::UpdateSocketData(Socket *sock) { sock->response_code = rescode_; }
